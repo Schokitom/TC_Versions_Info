bf0910fdc512bcf7cb35ebc460e25d5b630cce94
Schokitom <thomas.schoknecht@ewetel.net>
2026-04-21 16:41:57 +0200

    Update volltextsuche-v6.js

--- volltextsuche-v6.js
+++ volltextsuche-v6.js
@@ -86,10 +86,37 @@
       .catch(function() { return false; });
   }
 
-  function getDownloadUrl(fileId) {
+  function refreshToken() {
+    return new Promise(function(resolve) {
+      if (typeof workspaceAPI !== 'undefined' && workspaceAPI) {
+        workspaceAPI.requestPermission(function(token) {
+          if (token) {
+            accessToken = token;
+            console.log('[FTS] Token erneuert');
+            resolve(true);
+          } else if (typeof workspaceAPI.getAccessToken === 'function') {
+            workspaceAPI.getAccessToken(function(t) {
+              if (t) { accessToken = t; }
+              resolve(!!t);
+            });
+          } else { resolve(false); }
+        });
+        setTimeout(function() { resolve(false); }, 8000);
+      } else { resolve(false); }
+    });
+  }
+
+  function getDownloadUrl(fileId, _isRetry) {
     return fetch(PROXY_URL + '/core-fs/' + fileId + '/downloadurl?base=' + TC_BASE, {
       headers: { 'Authorization': 'Bearer ' + accessToken },
     }).then(function(r) {
+      if ((r.status === 401 || r.status === 403) && !_isRetry) {
+        console.log('[FTS] Token abgelaufen, erneuere...');
+        return refreshToken().then(function(ok) {
+          if (ok) return getDownloadUrl(fileId, true);
+          throw new Error('Token-Erneuerung fehlgeschlagen');
+        });
+      }
       if (!r.ok) throw new Error('DL-URL ' + r.status);
       return r.json();
     }).then(function(d) { return d.url; });
